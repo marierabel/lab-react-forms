5c4482d21f5604d756b1c733f44e0035edec5fec
Marie Rabel <marie.rabel0697@gmail.com>
2024-05-03 16:05:24 +0200

    Lab Fini

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,92 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [studentForm, setStudentForm] = useState({
+    fullName: "",
+    image: "",
+    phone: "",
+    email: "",
+    program: "--None--",
+    graduationYear: 0,
+    graduated: false,
+  });
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setStudents([...students, studentForm]);
+  }
+
+  function handleChange(event) {
+    setStudentForm({
+      ...studentForm,
+      [event.target.name]:
+        event.target.type === "checkbox"
+          ? event.target.checked
+          : event.target.value,
+    });
+  }
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form method="post" onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              onChange={handleChange}
+              value={studentForm.fullName}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              onChange={handleChange}
+              value={studentForm.image}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              onChange={handleChange}
+              value={studentForm.phone}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={handleChange}
+              value={studentForm.email}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              name="program"
+              onChange={handleChange}
+              value={studentForm.program}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,24 +111,28 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={handleChange}
+              value={studentForm.graduationYear}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              onChange={handleChange}
+              value={studentForm.graduated}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
